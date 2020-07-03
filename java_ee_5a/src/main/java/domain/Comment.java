ef8b6ad94117d36e979d3ab4e26ce456dd905928
Krzysiek <balas.krzysztof@gmail.com>
2020-07-03 10:30:59 +0200

    moved code

---
+++ java_ee_5a/src/main/java/domain/Comment.java
@@ -0,0 +1,58 @@
+package domain;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlTransient;
+import java.sql.Timestamp;
+
+@Entity
+@NamedQueries({
+  @NamedQuery(name="Comment.id", query="SELECT c FROM Comment c WHERE c.id=:commentId")
+})
+
+public class Comment {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private Timestamp timestamp;
+  private String content;
+
+  @ManyToOne
+  private Product product;
+
+  public int getId () {
+    return id;
+  }
+
+  public void setId (int id) {
+    this.id = id;
+  }
+
+  @XmlTransient
+  public Product getProduct () {
+    return product;
+  }
+
+  public void setProduct (Product product) {
+    this.product = product;
+  }
+
+  public Timestamp getTimestamp () {
+    return timestamp;
+  }
+
+  public void setTimestamp (Timestamp timestamp) {
+    this.timestamp = timestamp;
+  }
+
+
+  public String getContent () {
+    return content;
+  }
+
+  public void setContent (String content) {
+    this.content = content;
+  }
+}
